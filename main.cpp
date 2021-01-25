b39e8a0dc8548760ebbfc64ceae4e4665c8ef08b
Daniela Grothe <64636863+DGrothe-PhD@users.noreply.github.com>
2021-01-25 20:38:53 +0100

    stability amendment
    Added exception handling to catch an error if file words.txt is missing.

--- main.cpp
+++ main.cpp
@@ -19,7 +19,9 @@
 
 #include <iostream> //Command Prompt Input output
 #include <fstream> //Usage of Files
-#include <curses.h>
+#include <cstdlib>
+#include <string>
+#include <time.h>
 
 using namespace std;
 
@@ -28,13 +30,14 @@ string line, guessed, word, display;
 int numlines=0,inttries=0;
 ifstream myfile("words.txt"); //File containing all the words
 
+
 /*******************************
 ** Create all the Underscores **
 *******************************/
 void makeDisplay ()
 {
     display = "";
-    for (int k=0;k<string(word).size();k++)
+    for (int k=0;k<static_cast<int>(string(word).size());k++)
         display=display+"_"; //Add one underscore for every letter in the word
     guessed="_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _";
 }
@@ -44,20 +47,51 @@ void makeDisplay ()
 ******************************************************/
 string makeMan ()
 {
+    string prefix_normal="\n  ================|\n //               |\n ||               ";
+    string out_str = "";
+    string b3 = "\n ||             ";
+    string b1 = b3 + "  ";
+    string b2= b3 + " ";
+
     switch ( inttries )
     {
-        case 10:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||\n ||               ||    - *Snap!!!*\n ||              /||\\\n ||             //||\\\\\n ||            // || \\\\\n ||            *  ||  *\n ||              //\\\\\n ||             //  \\\\\n /\\            //    \\\\\n//\\\\         ***      ***\n/||\\\\\n_||_\\\\\n";
-        case 9:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||               ||\n ||              /||\\\n ||             //||\\\\\n ||            // || \\\\\n ||            *  ||  *\n ||              //\n ||             //\n ||            //\n /\\          ***\n//\\\\ \n/||\\\\ \n_||_\\\\\n";
-        case 8:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||               ||\n ||              /||\\\n ||             //||\\\\\n ||            // || \\\\\n ||            *  ||  *\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 7:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||               ||\n ||              /||\n ||             //||\n ||            // ||\n ||            *  ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 6:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||               ||\n ||               ||\n ||               ||\n ||               ||\n ||               ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 5:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||               ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 4:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||              _^_\n ||             / ..\\\n ||            [  _  ]\n ||             \\___/\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 3:return "\n  ================|\n //               |\n ||               |\n ||               |\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 2:return "\n  ================\n //\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 1:return "\n\n\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
-        case 0:return "\n\n\nCreated By Bernhard Breytenbach for educational purposes. 2008 - 1012\n\nPress * anytime to quit!\n\n\n\n\n\n\n\n\n\n\n\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 10:
+            out_str+= prefix_normal + "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]" +
+                b3 + "\\___/\n ||" + b1 + "||    - *Snap!!!*"+ b2 + "/||\\"+ b3 +
+                "//||\\\\\n ||            // || \\\\\n ||            *  ||  *"+
+                b2 + "//\\\\"+ b3 + "//  \\\\\n /\\            //    \\\\\n//\\\\         ***      ***\n/||\\\\\n_||_\\\\\n";
+        case 9:
+            out_str+= prefix_normal + "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]"+
+                b3 + "\\___/" + b1 + "||"+ b2 + "/||\\"+ b3 + "//||\\\\\n ||            // || \\\\\n ||            *  ||  *"+
+                b2 + "//"+ b3 + "//\n ||            //\n /\\          ***\n//\\\\ \n/||\\\\ \n_||_\\\\\n";
+        case 8:
+            out_str+= prefix_normal +  "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]"+
+                b3 + "\\___/" + b1 + "||"+ b2 + "/||\\"+ b3 +
+                "//||\\\\\n ||            // || \\\\\n ||            *  ||  *\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 7:
+            out_str+= prefix_normal +  "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]"+ b3
+                + "\\___/" + b1 + "||"+ b2 + "/||"+ b3 + "//||\n ||            // ||\n ||            *  ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 6:
+            out_str+= prefix_normal +  "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]"+ b3
+                + "\\___/" + b1 + "||" + b1 + "||" + b1 + "||" + b1 + "||" + b1 + "||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 5:
+            out_str+= prefix_normal +  "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]"+ b3
+                + "\\___/" + b1 + "||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 4:
+            out_str+= prefix_normal + "|" + b1 + "|"+ b2 + "_^_"+ b3 + "/ ..\\\n ||            [  _  ]"+ b3
+                + "\\___/\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 3:
+            out_str+= prefix_normal +  "|" + b1 + "|\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 2:
+            out_str+= "\n  ================\n //\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 1:
+            out_str+= "\n\n\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n ||\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        case 0:
+            out_str+= "\n\n\nCreated By Bernhard Breytenbach for educational purposes. 2008 - 1012\n\nPress * anytime to quit!\n\n\n\n\n\n\n\n\n\n\n\n /\\\n//\\\\\n/||\\\\\n_||_\\\\\n";
+        default:
+            ;
     }
+    return out_str;
 }
 
 /****************************************
@@ -68,18 +102,18 @@ void guesslet (char a)
     int correct=0,tried=0,validlet=0,k=0;
 
     for (k = 0; k < 51; k+=2) //Alphabetic Checks
-        if (alphabet[k]==a) //Check if letter is in alphabet
+        if (isalpha(a)) //Check if letter is in alphabet
         {
-            validlet=1; //Confirm valid entery
+            validlet=1; //Confirm valid entry
             if (guessed[k]!=a) //Check to see if the letter has been tried before
                 guessed[k]=a; //Save letter in String
                     else
                         tried=1; //Letter has been tried before
         }
-    
+
     if ((tried!=1) && (validlet==1)) //Check if letter is valid and hasn't been tried before
     {
-        for (k=0;k<string(word).size();k++)
+        for (k=0;k<static_cast<int>(string(word).size());k++)
             if (word[k]==a) //Check if the letter is in the word
             {
                 display[k]=a; //Change the underscore to the letter
@@ -93,14 +127,15 @@ void guesslet (char a)
 /************************************************
 ** Select a random word from the text document **
 ************************************************/
-int selectRandLine ()
+void selectRandLine ()
 {
-    int randline=0, wordlength=0, k=0;
+    try{
+        int randline=0, wordlength=0, k=0;
     string newword;
     //Generate random line
     randline = (rand()%numlines)+1;
     //Reset curser to first line
-    myfile.clear();    
+    myfile.clear();
     myfile.seekg(0, ios::beg);
     //Grab line from file
     for (int lineno = 0; getline (myfile,newword) && lineno < randline; lineno++);
@@ -112,6 +147,13 @@ int selectRandLine ()
             if (alphabet[k]==newword[i]) //Check if letter is in alphabet
                 word=word+newword[i];
     }
+    }
+    catch(const std::exception &e){
+        cout << "Some fatal exception. File words.txt missing?";
+    }
+    catch(int j){
+        cout << "Error occurred in the selectRandLine";
+    }
 }
 
 /*******************
@@ -119,32 +161,45 @@ int selectRandLine ()
 *******************/
 int main ()
 {
-    initscr();
-    char a; //Guessing letter
-    srand((unsigned)time(0)); //Seed RNG
-    while (std::getline(myfile, line)) ++numlines; //Open the word list and count how many words are present.
-    
-    while (a!='*') //Continuous loop with exit number 2.
-    {
-        selectRandLine (); //Select a random word from the file
-        makeDisplay(); //Make the correct number of underscores
-        inttries = 0; //Reset number of tries
-        while (display!=word) //Exit loop when word is guessed correctly.
-        {
-            cout << makeMan (); //Draw ASCII Man d-_-b
-            cout << "Please guess a letter!\nWord: " << display << "\nGuessed: " << guessed << "\nTries left:" << 10-inttries << "\n";
-            cin >> a; //Read user input
-            guesslet(a); //Check if letter is valid and in word
-            if (a=='*') inttries=10; //Check for quit character
-            if (inttries>=10) display=word; //Meet condition to exit loop when tries expire
+    try{
+        if(!myfile.is_open()){
+            cout << "File not found" << std::endl;
+            throw 404;
         }
-        cout << makeMan (); //Draw ASCII Man d-_-b
-        //Message when user loses.
-        if (inttries>=10) cout << "\nYou lost! The word was \"" << word << "\".\nPress any key to play again, or * to quit.\n\n";
-        //Message when user wins!
-        if (inttries<10) cout << "\nYou won! The word is \"" << word << "\"!\nPress any key to play again, or * to quit.\n\n";
-        cin >> a; //Read user input
-    }
 
+        char a; //Guessing letter
+        srand((unsigned)time(0)); //Seed RNG
+        while (std::getline(myfile, line)) ++numlines; //Open the word list and count how many words are present.
+
+        while (a!='*') //Continuous loop with exit number 2.
+        {
+            selectRandLine (); //Select a random word from the file
+            makeDisplay(); //Make the correct number of underscores
+            inttries = 0; //Reset number of tries
+            while (display!=word) //Exit loop when word is guessed correctly.
+            {
+                cout << makeMan (); //Draw ASCII Man d-_-b
+                cout << "Please guess a letter!\nWord: " << display << "\nGuessed: " << guessed << "\nTries left:" << 10-inttries << "\n";
+                cin >> a; //Read user input
+                guesslet(a); //Check if letter is valid and in word
+                if (a=='*') inttries=10; //Check for quit character
+                if (inttries>=10) display=word; //Meet condition to exit loop when tries expire
+            }
+            cout << makeMan (); //Draw ASCII Man d-_-b
+            //Message when user loses.
+            if (inttries>=10) cout << "\nYou lost! The word was \"" << word << "\".\nPress any key to play again, or * to quit.\n\n";
+            //Message when user wins!
+            if (inttries<10) cout << "\nYou won! The word is \"" << word << "\"!\nPress any key to play again, or * to quit.\n\n";
+            cin >> a; //Read user input
+        }
+    }
+    catch(int j){
+        if(j==404){
+            cout << "File words.txt was not found.";
+        }
+        else {
+            cout << "Error number "<< j << " occurred in main.";
+        }
+    }
     return 0;
 }
